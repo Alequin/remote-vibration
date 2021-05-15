851da0e65b043c4806fc0bfed925ac6f89346af6
Alequin <j-cox03@hotmail.com>
2021-05-15 17:15:51 +0100

    used smartBanner for the ad banner

--- src/shared/ad-banner.js
+++ src/shared/ad-banner.js
@@ -1,9 +1,9 @@
 import { AdMobBanner } from "expo-ads-admob";
 import { noop } from "lodash";
 import React from "react";
+import { StyleSheet, View } from "react-native";
 import { isSmallScreen } from "../utilities/is-small-screen";
 import { useIsKeyboardVisible } from "./use-is-keyboard-visible";
-import { bannerUnitId } from "../../secrets.json";
 
 // Test admob unit id
 const testBannerId = "ca-app-pub-3940256099942544/6300978111";
@@ -13,11 +13,21 @@ export const AdBanner = () => {
   // On small screens when the keyboard is visible some elements do not fit on the page
   if (isSmallScreen() && isKeyboardVisible) return null;
   return (
-    <AdMobBanner
-      bannerSize="fullBanner"
-      adUnitID={bannerUnitId || testBannerId}
-      servePersonalizedAds // true or false
-      onDidFailToReceiveAdWithError={noop}
-    />
+    <View style={ViewStyles.adContainer}>
+      <AdMobBanner
+        bannerSize="smartBannerPortrait"
+        adUnitID={testBannerId}
+        servePersonalizedAds={false}
+        onDidFailToReceiveAdWithError={noop}
+      />
+    </View>
   );
 };
+
+const ViewStyles = StyleSheet.create({
+  adContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+});
