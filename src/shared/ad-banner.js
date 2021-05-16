0babc4657018ab612f15cfc615a23d061f226aff
Alequin <j-cox03@hotmail.com>
2021-05-16 16:08:56 +0100

    Added a background to the ads so when they fail to load it looks okay

--- src/shared/ad-banner.js
+++ src/shared/ad-banner.js
@@ -1,17 +1,17 @@
 import { AdMobBanner } from "expo-ads-admob";
-import { noop } from "lodash";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { isSmallScreen } from "../utilities/is-small-screen";
 import { useIsKeyboardVisible } from "./use-is-keyboard-visible";
 import { bannerUnitId } from "../../secrets.json";
+import { darkSpaceCadet } from "../utilities/colours";
 
-export const AdBanner = (environment) => {
+export const AdBanner = ({ environment }) => {
   const isKeyboardVisible = useIsKeyboardVisible();
   // On small screens when the keyboard is visible some elements do not fit on the page
   if (isSmallScreen() && isKeyboardVisible) return null;
   return (
-    <View style={ViewStyles.adContainer}>
+    <View style={{ ...ViewStyles.adContainer }}>
       <AdMobBanner
         bannerSize="smartBannerPortrait"
         adUnitID={
@@ -20,7 +20,6 @@ export const AdBanner = (environment) => {
             : "ca-app-pub-3940256099942544/6300978111" // Test admob unit id
         }
         servePersonalizedAds={false}
-        onDidFailToReceiveAdWithError={noop}
       />
     </View>
   );
@@ -31,5 +30,6 @@ const ViewStyles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
+    backgroundColor: darkSpaceCadet,
   },
 });
