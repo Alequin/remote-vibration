903544a490fe1e7e9474f69dbe167a2ee1b7a76a
Alequin <j-cox03@hotmail.com>
2021-05-23 19:14:18 +0100

    made a script to create the app.json so it can be gitignored to hide secrets

---
+++ app.json-generator.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const { googleMobileAdsAppId } = require("./secrets.json");
+
+const appVersionNumber = 6;
+
+fs.writeFileSync(
+  __dirname + "/app.json",
+  JSON.stringify(
+    {
+      expo: {
+        name: "Vibration Control! Help yourself and others relax",
+        slug: "vibration-remote-send-vibrations-to-other-devices", // This should never be changed
+        version: `${appVersionNumber}.0.0`,
+        orientation: "portrait",
+        icon: "./assets/icon.png",
+        updates: {
+          fallbackToCacheTimeout: 0,
+        },
+        assetBundlePatterns: ["**/*"],
+        ios: {
+          supportsTablet: true,
+        },
+        android: {
+          package: "com.just_for_fun.remote_vibration",
+          versionCode: appVersionNumber,
+          config: {
+            googleMobileAdsAppId,
+          },
+          adaptiveIcon: {
+            foregroundImage: "./assets/adaptive-icon.png",
+            backgroundColor: "#FFFFFF",
+          },
+          permissions: ["VIBRATE", "ACCESS_WIFI_STATE"],
+        },
+      },
+    },
+    null,
+    2
+  )
+);
