0c66715dd94f61bf848bb2197408c52b93745355
Alequin <j-cox03@hotmail.com>
2021-06-20 12:40:20 +0100

    increased version

--- app.json-generator.js
+++ app.json-generator.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const { googleMobileAdsAppId } = require("./secrets.json");
 
-const appVersionNumber = 8;
+const appVersionNumber = 9;
 
 fs.writeFileSync(
   __dirname + "/app.json",
